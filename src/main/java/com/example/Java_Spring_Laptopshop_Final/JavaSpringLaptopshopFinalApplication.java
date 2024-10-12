09d3ae99ef94e2fef8997f4681717e673d0a7801
Vinh3 <101193855+vinh333@users.noreply.github.com>
2024-10-12 09:37:36 +0700

    First commit

---
+++ src/main/java/com/example/Java_Spring_Laptopshop_Final/JavaSpringLaptopshopFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java_Spring_Laptopshop_Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringLaptopshopFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringLaptopshopFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Java_Spring_Laptopshop_Final/JavaSpringLaptopshopFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_Spring_Laptopshop_Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLaptopshopFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
